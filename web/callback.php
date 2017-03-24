32a939964882eccf0736b0c4108aaada1e0eb01d
ytcreater <fm803fm@yahoo.co.jp>
2017-03-24 15:24:00 +0900

    Update callback.php

--- web/callback.php
+++ web/callback.php
@@ -135,7 +135,7 @@ if ($text == 'はい') {
     "altText" => "こんにちわ 何かご用ですか？（はい／いいえ）",
     "template" => [
         "type" => "confirm",
-        "text" => "こんにちわ 何かご用ですか？",
+        "text" => "変態ですか？",
         "actions" => [
             [
               "type" => "message",
